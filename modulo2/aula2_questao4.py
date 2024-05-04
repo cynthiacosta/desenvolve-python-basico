a33721f7a63d5d7deafaf8a95bc4c1ee5ca5f152
cynthiacosta <165850837+cynthiacosta@users.noreply.github.com>
2024-05-04 11:22:46 -0300

    Add files via upload

---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,8 @@
+juros = 1.01
+saldo = 500.0
+
+for _ in range(3):
+    saldo *= juros
+
+print("Após 3 meses, meu novo saldo é")
+print(saldo)
---
+++ modulo2/aula2_quetao3.py
@@ -0,0 +1,14 @@
+v1 = 10
+v2 = 20
+
+print("Antes da troca:")
+print("v1 =", v1)
+print("v2 =", v2)
+
+auxiliar = v1
+v1 = v2
+v2 = auxiliar
+
+print("Depois da troca:")
+print("v1 =", v1)
+print("v2 =", v2)
