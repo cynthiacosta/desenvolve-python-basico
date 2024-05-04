f87722835d07e6fb4e20f3deb5ba99a6d147e25c
cynthiacosta <165850837+cynthiacosta@users.noreply.github.com>
2024-05-04 10:49:08 -0300

    Add files via upload

---
+++ modulo 2/aula2_questao2.py
@@ -0,0 +1,12 @@
+texto = "o resultado é"
+
+valor1 = 10
+
+valor2 = 3.5
+
+resultado = valor1 + valor2
+
+print("texto:", texto, "- Tipo:", type(texto))
+print("valor1:", valor1, "- Tipo:", type(valor1))
+print("valor2:", valor2, "- Tipo:", type(valor2))
+print("resultado:", resultado, "- Tipo:", type(resultado))
