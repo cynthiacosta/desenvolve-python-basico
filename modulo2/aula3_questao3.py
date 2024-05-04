d84d6893a7cc0f2a08f873afc40039abe1be505f
cynthiacosta <165850837+cynthiacosta@users.noreply.github.com>
2024-05-04 11:49:29 -0300

    Add files via upload

---
+++ modulo2/aula3_questao3.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade: "))
+
+jogou_tres_jogos = input("Já jogou pelo menos 3 jogos de tabuleiro? ").lower() == "true"
+
+jogos_vencidos = int(input("Quantos jogos já venceu? "))
+
+apto = 16 <= idade <= 18 and jogou_tres_jogos and jogos_vencidos >= 1
+
+print(f"Apto para ingressar no clube de jogos de tabuleiro: {apto}")
